ec107a9ca4144df4a4666eccd7acc4dc0c4f26e7
Nguyen Duc Tai <ductaaii.2002@gmail.com>
2025-02-10 06:15:29 +0700

    Chỉnh sửa cấu hình

---
+++ React_FE/src/App.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import Dashboard from "./pages/Dashboard";
+import HomePage from "./pages/HomePage";
+import ProtectedRoute from "./components/ProtectedRoute";
+import { AuthProvider, useAuth } from "./context/AuthContext.jsx";
+
+const DefaultRoute = () => {
+  const { user } = useAuth();
+  return user ? <HomePage /> : <Navigate to="/login" />;
+};
+
+
+function App() {
+  return (
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<DefaultRoute />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/dashboard" element={<ProtectedRoute role="Admin"><Dashboard /></ProtectedRoute>} />
+        </Routes>
+      </Router>
+    </AuthProvider>
+  );
+}
+
+export default App;
