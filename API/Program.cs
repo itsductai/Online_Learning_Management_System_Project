bf2b0b8d31af1c4739cbb4e5833118a2dd3ff9c8
Nguyen Duc Tai <ductaaii.2002@gmail.com>
2025-02-16 22:04:54 +0700

    Cấu hình JWT trong Program.cs.

--- API/Program.cs
+++ API/Program.cs
@@ -2,8 +2,29 @@
 using Data.Models;
 using API.Services;
 using API.Repositories;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Cấu hình Authentication JWT
+var jwtSettings = builder.Configuration.GetSection("JwtSettings");
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = jwtSettings["Issuer"],
+            ValidAudience = jwtSettings["Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings["Key"]))
+        };
+    });
 
 // Thêm dịch vụ Controllers (Web API)
 
@@ -11,6 +32,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<JwtService>();
 
 // Đăng ký DbContext với Connection String từ appsettings.json
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
@@ -52,6 +74,7 @@ if (app.Environment.IsDevelopment())
 }
 
 // Bật Authorization (bảo vệ API)
+app.UseAuthentication();
 app.UseAuthorization();
 
 // Định nghĩa API Controllers
@@ -59,6 +82,5 @@ app.MapControllers();
 
 // Chạy ứng dụng
 app.Run();
-
 
 
